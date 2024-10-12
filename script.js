6aea7a32d5b9ad60cc0fcaa5ccb049530c3ef0f8
alaham91 <alaahamzeh56@gmail.com>
2024-10-12 23:57:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert("FIXIT offers expert repair services for all kinds of appliances. Stay tuned for more details!");
+});
